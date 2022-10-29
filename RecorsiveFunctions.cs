8e1752ecefd47c1b2ebff8e56383e77a7c492482
liran bratt <95234790+LiranBratt2121@users.noreply.github.com>
2022-10-29 18:57:25 +0300

    Update RecorsiveFunctions.cs

--- RecorsiveFunctions.cs
+++ RecorsiveFunctions.cs
@@ -8,6 +8,46 @@ namespace ConsoleApp11
 {
     internal class RecorsiveFunctions
     {
-
+        public int GetFullNum(int n) //q1
+        {
+            if (n == 0)
+                return 0;
+            return n + GetFullNum(n - 1);
+        }
+        public int MultPosNums(int n) //q2
+        {
+            if (n == 1)
+                return 1;
+            if (n % 2 != 0)
+                return n * MultPosNums(n - 1);
+            else
+                return MultPosNums(n - 1);
+        }
+        public int GetFactorial(int n) //q3
+        {
+            if (n == 1)
+                return 1;
+            return n * GetFactorial(n - 1);
+        }
+        public int GetAmountOfNums(long n) //q4
+        {
+            if (n < 10)
+                return 1;
+            return 1 + GetAmountOfNums(n / 10);
+        }
+        public int GetAmountOfEvenNums(long n) //q5
+        {
+            if (n < 10 && n % 2 == 0)
+                return 1;
+            if (n % 2 == 0)
+                return 1 + GetAmountOfEvenNums(n / 10);
+            return GetAmountOfEvenNums(n / 10);
+        }
+        public int GetLeftNumber(int n) //q6
+        {
+            if (n < 10)
+                return n;
+            return GetLeftNumber(n / 10);
+        }
     }
 }
